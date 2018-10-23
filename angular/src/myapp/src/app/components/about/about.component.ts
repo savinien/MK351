bdb818bc92384bbf9c1f6fc0c03142a7830b549a
Jean Savinien <jean.savinien@gmail.com>
2018-10-23 11:58:12 +0200

    finished myapp with routing

--- angular/src/myapp/src/app/components/about/about.component.ts
+++ angular/src/myapp/src/app/components/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-about',
@@ -9,12 +10,17 @@ export class AboutComponent implements OnInit {
   title: string = "About our app";
   elements: string[] = ["element 1", "element 2", "element 3"];
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
   }
 
   getTitle(){
     return this.title
+  }
+
+  backHome(){
+    this.router.navigate(['']);
+    // this.router.navigateByUrl('');
   }
 }
