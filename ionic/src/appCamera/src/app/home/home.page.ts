3d982ee708542d6e1b075293978db132b9dadff5
Jean Savinien <jean.savinien@gmail.com>
2018-11-21 17:42:59 +0100

    appCamera done

--- ionic/src/appCamera/src/app/home/home.page.ts
+++ ionic/src/appCamera/src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { Camera, CameraOptions } from '@ionic-native/camera';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 
+declare var window;
 
 @Component({
   selector: 'app-home',
@@ -9,23 +10,32 @@ import { Camera, CameraOptions } from '@ionic-native/camera';
 })
 export class HomePage {
 
-myPicture: any;
+myPicture: string;
+error: string;
 
 constructor(private camera: Camera) { }
+
+test(){
+  console.log("BLA");
+}
+
 
 takePicture(){
   const options: CameraOptions = {
     quality: 70,
     destinationType: this.camera.DestinationType.FILE_URI,
     encodingType: this.camera.EncodingType.JPEG,
-    mediaType: this.camera.MediaType.PICTURE
+    mediaType: this.camera.MediaType.PICTURE,
+    saveToPhotoAlbum: true
   }
 
   this.camera.getPicture(options).then((imageData) => {
-   this.myPicture = 'data:image/jpeg;base64,' + imageData;
-   console.log("picture taken:", imageData);
+   //this.myPicture = 'data:image/jpeg;base64,' + imageData;
+   this.myPicture = window.Ionic.WebView.convertFileSrc(imageData);
+   console.log("BLA - picture taken:", imageData);
   }, (err) => {
-   console.log("error occured");
+    this.error = err;
+   console.log("BLA - error occured: ", err);
   });
 }
 
