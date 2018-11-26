004f8e2799bf88034bc99aef4cf6913c6aca15a9
Jean Savinien <jean.savinien@gmail.com>
2018-11-26 11:17:27 +0100

    adding geolocation

--- ionic/src/mapApp/src/app/home/home.page.ts
+++ ionic/src/mapApp/src/app/home/home.page.ts
@@ -1,8 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss']
 })
-export class HomePage {}
+export class HomePage implements OnInit {
+
+  myLatLng: Coordinates = {lat:null, lng:null};
+
+  constructor(private geolocation: Geolocation) {
+  }
+
+  ngOnInit(){
+    this.loadMap();
+  }
+
+  async loadMap() {
+    this.myLatLng = await this.getLocation();
+    console.log("GPS coordinates: ", this.myLatLng);
+  }
+
+  private async getLocation() {
+    const rta = await this.geolocation.getCurrentPosition();
+    return {
+      lat: rta.coords.latitude,
+      lng: rta.coords.longitude
+    };
+  }
+
+}
+
+interface Coordinates {
+  lat: number,
+  lng: number
+}
